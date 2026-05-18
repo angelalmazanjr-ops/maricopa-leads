9eaedc0cd3a0e85e37595ff7173bf9f01e5cebf9
angelalmazanjr-ops <angelalmazanjr@gmail.com>
2026-05-17 18:52:26 -0700

    Update fetch.py

--- scraper/fetch.py
+++ scraper/fetch.py
@@ -42,7 +42,7 @@ def make_session():
 def build_url(code, dfrom, dto, start=0):
     b = dfrom.replace("/", "%2F")
     e = dto.replace("/", "%2F")
-    return f"{RECORDER_SEARCH}?rec={start}&suf=&nm=&bdt={b}&edt={e}&cde={code}&max=100&res=True&doc1={code}"
+    return f"{RECORDER_SEARCH}?rec={start}&suf=&nm=&bdt={b}&edt={e}&cde={code}max=20&res=True&doc1={code}&doc2=&doc3=&doc4=&doc5="
 
 def fetch_page(session, url):
     for i in range(3):
